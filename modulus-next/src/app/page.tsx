204c7610c7e113b043c956c31554ee71ff2fa8d5
Muslum Abd Ali <muslumabdali1234@gmail.com>
2025-07-26 19:39:23 -0400

    css fixes

--- modulus-next/src/app/page.tsx
+++ modulus-next/src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import "../styles/globals.css";
+import "/styles/globals.css";
 import Head from 'next/head';
 import Script from 'next/script';
 import Link from 'next/link';
@@ -32,7 +32,6 @@ export default function LandingPage() {
         <title>Modulus</title>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="stylesheet" href="/CSS/output.css" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <style>
         @import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap');
